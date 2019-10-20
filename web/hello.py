b3e4c6997795f1bd85892edab4e305e62905e2cd
Raquel <raquelmarcolino25@gmail.com>
2019-10-20 17:55:25 -0300

    Arquivos para o deploy.

--- web/hello.py
+++ web/hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, send_from_directory
 from app import ai
 
@@ -28,4 +29,5 @@ def next_move():
     return jsonify({"move" : list(action)})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
